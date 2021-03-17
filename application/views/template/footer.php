ce847bd851b245ee606e8b9dfc6bec5f2be7aca9
Akhirwan Novendi <akhirwan.novendi@gmail.com>
2021-03-17 11:16:51 +0700

    initial commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,41 @@
+		<footer class="tm-footer text-center">
+			<p>Copyright &copy; 2020 Simple House 
+            
+            | Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
+		</footer>
+	</div>
+	<script src="<?php echo base_url('assets/public/js/jquery.min.js')?>"></script>
+	<script src="<?php echo base_url('assets/public/js/parallax.min.js')?>"></script>
+	<script>
+		$(document).ready(function(){
+			// Handle click on paging links
+			$('.tm-paging-link').click(function(e){
+				e.preventDefault();
+				
+				var page = $(this).text().toLowerCase();
+				$('.tm-gallery-page').addClass('hidden');
+				$('#tm-gallery-page-' + page).removeClass('hidden');
+				$('.tm-paging-link').removeClass('active');
+				$(this).addClass("active");
+			});
+		});
+        
+        $(document).ready(function(){
+            var acc = document.getElementsByClassName("accordion");
+            var i;
+            
+            for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+                });
+            }	
+        });
+    </script>
+</body>
+</html>
